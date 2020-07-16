327100220da3df6ea9c982ec12ec5cd5321da2e4
Orian Galklowicz <oriangalklowicz@macbook-pro.home>
2020-07-16 21:06:40 +0300

    Add application state. Add util functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,25 @@ import React from 'react'
 import './App.scss'
 import Table from './components/Table'
 import { managers, accounts } from './fakeDB'
-import { parseAccountsData, parseManagersData } from './util'
+import { parseAccountsData, parseManagersData, findAllEmployeesOfManager, getManagerById } from './util'
 
 function App() {
-  const paredAccounts = parseAccountsData(accounts)
-  const paredManagers = parseManagersData(managers, accounts)
+  const [paredManagers, setManagers] = React.useState(parseManagersData(managers, accounts, ''))
+  const [paredAccounts, setAccounts] = React.useState(parseAccountsData(accounts, [], ''))
+
+  const handleManagerClick = (managerId: string) => {
+    const selectedManager = getManagerById(managerId, managers)
+    const { name } = selectedManager || { name: '' }
+    const managerEmployees = findAllEmployeesOfManager([name], managers)
+
+    setAccounts(parseAccountsData(accounts, managerEmployees, name))
+    setManagers(parseManagersData(managers, accounts, name))
+  }
 
   return (
     <div className="App container">
       <div className="managers">
-        <Table tableData={paredManagers} tableColumns={['id', 'name', 'manager']} />
+        <Table tableData={paredManagers} tableColumns={['id', 'name', 'manager']} onRowClick={handleManagerClick} />
       </div>
       <div className="accounts">
         <Table tableData={paredAccounts} tableColumns={['id', 'name', 'account manager', 'status']} />
--- src/util.tsx
+++ src/util.tsx
@@ -5,20 +5,58 @@ const isManagerWithNoAccounts = (managerName: string, accounts: Account[]) => {
   return !accountFound
 }
 
-const parseAccountsData = (accounts: Account[]) => {
+const isHighlightedAccount = (account: Account, managerEmployees: string[], selectedManager: string) => {
+  const isAccountManagedBySelected = managerEmployees.findIndex((name: string) => name === account.accountManager)
+  const isAccountOfSelected = account.accountManager === selectedManager
+
+  return isAccountManagedBySelected !== -1 || isAccountOfSelected
+}
+
+const parseAccountsData = (accounts: Account[], managerEmployees: string[], selectedManager: string) => {
   return accounts.map((account) => {
     const disabled = account.status === Status.closed
+    const isHighlighted = isHighlightedAccount(account, managerEmployees, selectedManager)
 
-    return { data: { ...account }, metadata: { disabled } }
+    return { data: { ...account }, metadata: { disabled, ishighlighted: isHighlighted } }
   })
 }
 
-const parseManagersData = (managers: Manager[], accounts: Account[]) => {
+const parseManagersData = (managers: Manager[], accounts: Account[], selectedManager: string) => {
   return managers.map((manager) => {
     const noAccounts = isManagerWithNoAccounts(manager.name, accounts)
+    const selected = selectedManager === manager.name
 
-    return { data: { ...manager }, metadata: { noaccounts: noAccounts } }
+    return { data: { ...manager }, metadata: { noaccounts: noAccounts, selected} }
   })
 }
 
-export { parseAccountsData, parseManagersData }
+const findAllEmployeesOfManager = (managersArray: string[], managers: Manager[]) => {
+  let result: Array<string> = []
+
+  const recursiveSearch = (managersArray: string[], managers: Manager[]) => {
+    if (managersArray.length === 0) {
+      return result
+    }
+
+    managersArray.forEach((managerName) => {
+      const managersFound = managers
+        .filter((manager) => {
+          return manager.manager === managerName
+        })
+        .map((manager) => manager.name)
+      result = [...result, ...managersFound]
+
+      recursiveSearch(managersFound, managers)
+    })
+  }
+
+  recursiveSearch(managersArray, managers)
+
+  return result
+}
+
+const getManagerById = (managerId: string, managers: Manager[]) => {
+  return managers.find((manager) => manager.id === managerId)
+}
+
+export { parseAccountsData, parseManagersData, findAllEmployeesOfManager, getManagerById }
